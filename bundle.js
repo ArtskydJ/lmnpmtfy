450e0a7168fffa6ccd5f1eb3c4fa1b6251efecb9
Joseph Dykstra <josephdykstra@gmail.com>
2015-06-02 22:41:51 -0500

    Moved copy-link-thing to under the search bar

--- bundle.js
+++ bundle.js
@@ -189,12 +189,13 @@ module.exports = function copyableElement() {
 	var copyable = document.getElementById('copyable')
 	if (!copyable) {
 		var div = document.createElement('div')
-		div.innerHTML = '<br><br>Press Ctrl + C to copy: '
+		div.id = 'copyable-div'
+		div.innerHTML = 'Press Ctrl + C to copy: '
 
 		copyable = document.createElement('input')
 		copyable.id = 'copyable'
 
-		document.body.appendChild(div)
+		document.getElementById('site-search').appendChild(div)
 		div.appendChild(copyable)
 	}
 	return copyable
