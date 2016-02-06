711fa6e1e9ebf83efda13fb6cc8c03b1c869083d
Joseph Dykstra <josephdykstra@gmail.com>
2016-02-05 18:59:54 -0600

    Fix fake hover

--- js/troll.js
+++ js/troll.js
@@ -1,7 +1,6 @@
 var rangeInterval = require('range-interval')
 var qs = require('querystring')
 var cursorElement = require('./element-cursor.js')
-var calculate = require('./calculate.js')
 
 var npmSearch = document.getElementById('npm-search')
 var siteSearchText = document.getElementById('site-search')
@@ -15,9 +14,12 @@ var cursorRepeatOpts = {
 }
 
 module.exports = function troll(text) {
-	init()
+	// If you go 'back' to this page, it won't troll you again
+	window.location.replace(window.location.href + '#')
+
 	moveCursorToInput(function () {
-		clickInput()
+		siteSearchText.focus() // Not sure this does anything
+
 		typeInput(text, function () {
 			moveCursorToSearchButton(function () {
 				setTimeout(clickSearchButton, 100, text)
@@ -27,49 +29,53 @@ module.exports = function troll(text) {
 }
 
 function moveCursorToInput(cb) {
-	var next = calculate(cursorElement(), {
-		topPx: [ 0, 80 ],
-		leftVw: [ 0, 50 ]
-	})
-	rangeInterval(cursorRepeatOpts, next, cb)
-}
+	var element = cursorElement()
 
-function clickInput() {
-	siteSearchText.focus()
-	npmSearch.className = 'hover'
+	rangeInterval(cursorRepeatOpts, function (ratio) {
+		element.style.top = resolveRatio(ratio, 0, 80) + 'px'
+		element.style.left = resolveRatio(ratio, 0, 50) + 'vw'
+
+		if (ratio > 0.7) {
+			npmSearch.className = 'hover'
+		}
+	}, cb)
 }
 
 function typeInput(text, cb) {
-	rangeInterval({
+	var textRepeatOpts = {
 		start: 0,
 		end: text.length,
 		step: 1,
 		interval: 200
-	}, function stepInput(index) {
+	}
+
+	rangeInterval(textRepeatOpts, function stepInput(index) {
 		siteSearchText.value = text.slice(0, index)
 	}, cb)
 }
 
 function moveCursorToSearchButton(cb) {
-	var next = calculate(cursorElement(), {
-		leftPx: [ 0, -80 ],
-		leftVw: [ 50, 100 ]
-	})
+	var element = cursorElement()
 
-	rangeInterval(cursorRepeatOpts, stepCursor, cb)
-
-	function stepCursor(fraction) {
-		next(fraction)
-		if (fraction > 0.95) {
+	rangeInterval(cursorRepeatOpts, function stepCursor(ratio) {
+		element.style.left = calc(
+			resolveRatio(ratio, 0, -80) + 'px',
+			resolveRatio(ratio, 50, 100) + 'vw'
+		)
+		if (ratio > 0.95) {
 			siteSearchSubmit.className = 'hover'
 		}
-	}
-}
-
-function init() {
-	window.location.replace(window.location.href + '#') // If you go 'back' to this page, it won't troll you again.
+	}, cb)
 }
 
 function clickSearchButton(text) {
 	window.location.assign('https://npmjs.com/search?' + qs.stringify({ q: text }))
+}
+
+function calc(a, b) {
+	return 'calc(' + a + ' + ' + b + ')'
+}
+
+function resolveRatio(ratio, low, high) {
+	return Math.round(low + (high - low) * ratio)
 }
